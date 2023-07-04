afbfa66d46d383bd9d7535ebf728969373852d45
Vyppal <86544866+Vyppal@users.noreply.github.com>
2023-07-04 21:48:09 +0800

    accurate sprites render

--- src/main.cpp
+++ src/main.cpp
@@ -21,7 +21,51 @@ BoardData boardData{BOARD_WIDTH, BOARD_HEIGHT, 4};
 Board board{boardData, &gameHandler};
 
 
-	
+std::string GetSprite(Tile *tile) {
+  TileInformation tileInfo = tile->GetTileInformation();
+  std::string imagePath = "";
+  /*if (!tile->GetIsOpen()) {
+    imagePath += "inactive.png";
+  }
+  else */ if (tileInfo.isMine) {
+    imagePath += "mine.png";
+  }
+  else {
+    switch (tileInfo.adjacentMineCount) {
+      case 0:
+        imagePath += "zero.png";
+       break;
+      case 1:
+        imagePath += "one.png";
+       break;
+      case 2:
+        imagePath += "two.png";
+       break;
+      case 3:
+        imagePath += "three.png";
+       break;
+      case 4:
+        imagePath += "four.png";
+       break;
+      case 5:
+        imagePath += "five.png";
+       break;
+      case 6:
+        imagePath += "six.png";
+       break;
+      case 7:
+        imagePath += "seven.png";
+       break;
+      case 8:
+        imagePath += "eight.png";
+       break;
+      default:
+        imagePath += "zero.png";
+       break;
+    }
+  }
+  return imagePath;
+}
 
 
 
@@ -39,14 +83,13 @@ int main(int argc, char* argv[]) {
   windowSurface = SDL_GetWindowSurface( window );
   SDL_Event windowEvent;
   if( !( IMG_Init( IMG_INIT_PNG ) & IMG_INIT_PNG ) ) {
-    std::cout << "Could not create window: " << IMG_GetError( ) << std::endl;
+    std::cout << "Could not create window: " << IMG_GetError() << std::endl;
     return 1;
   }
 
-  imageSurface = IMG_Load("res/textures/mine.png");
-  if ( NULL == imageSurface ){
-    std::cout << "SDL could not load image! SDL Error: " << SDL_GetError( ) << std::endl;
-  }
+  // if ( NULL == imageSurface ){
+  //   std::cout << "SDL could not load image! SDL Error: " << SDL_GetError() << std::endl;
+  // }
 
   while (true) {
     if (SDL_PollEvent(&windowEvent)) {
@@ -57,8 +100,8 @@ int main(int argc, char* argv[]) {
 
     for (auto row : board.GetBoard()) {
       for (auto tile : row) {
-        SDL_Surface *imageSurface1 = IMG_Load("res/textures/mine.png");
-        SDL_BlitScaled( imageSurface, NULL, windowSurface, new SDL_Rect{tile.GetXPos() * TILE_SIZE, tile.GetYPos() * TILE_SIZE, TILE_SIZE, TILE_SIZE} );
+        SDL_Surface *imageSurface1 = IMG_Load(("res/textures/" + GetSprite(&tile)).c_str());
+        SDL_BlitScaled(imageSurface1, NULL, windowSurface, new SDL_Rect{tile.GetXPos() * TILE_SIZE, tile.GetYPos() * TILE_SIZE, TILE_SIZE, TILE_SIZE});
       }
     }
     SDL_UpdateWindowSurface( window );
